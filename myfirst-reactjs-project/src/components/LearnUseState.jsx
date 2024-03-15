a3ba67110c55d619a2c2535aa058af474cf7bdd8
Refat Jamil <rifatjamil54@gmail.com>
2024-03-15 23:46:02 +0600

    day 04 - LearnUseState - use prevState for updateing position value

--- myfirst-reactjs-project/src/components/LearnUseState.jsx
+++ myfirst-reactjs-project/src/components/LearnUseState.jsx
@@ -4,19 +4,35 @@ import { useState } from 'react'
 
 export const LearnUseState=() => {
 
-  const [num, func] = useState(5)
+  const [num, setNum] = useState(5)
 
   const changeNumber = () => {
-    func(9)
+    setNum(num+2)
   }
+
+  const [position, setPosition] = useState({x:1, y:2})
+
+  const updatePosition_x = () => {
+    setPosition(prevState => (console.log(prevState),{ ...prevState, x: prevState.x + 2 }));
+  }
+  
+  const updatePosition_y = () => {
+    setPosition(prevState => (console.log(prevState),{ ...prevState, y: prevState.y + 2 }));
+  }
+  
 
   return (
     <>
     <h1>LearnUseState</h1>
     <p>Number: {num}</p>
     <button onClick={changeNumber}>Change Number</button>
+    <p>Position X : {position.x} </p>
+    <p>Position Y : {position.y } </p>
 
-
+    <button onClick={updatePosition_x}>updatePosition X</button>
+    <button onClick={updatePosition_y}>updatePosition Y</button>
+<br />
+    -------------------------
     </>
   )
 }
